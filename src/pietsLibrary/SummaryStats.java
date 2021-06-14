c40eaa649842ccb0243d207657a6a9892cf20d5d
Piet Snot <pietmuis@gmail.com>
2021-06-14 12:26:58 +0200

    Piets bibliotheek (vereenvoudigd)

--- src/pietsLibrary/SummaryStats.java
+++ src/pietsLibrary/SummaryStats.java
@@ -7,22 +7,42 @@
 package pietsLibrary;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Function;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.mapping;
 import static java.util.stream.Collectors.toList;
 
 /**
  *
  * @author Piet
- * @param <T>
+ * @param <T>  generic type of the elements
  */
-public class SummaryStats<T extends Number> {
+public class SummaryStats<T> {
     
-    List<T> list = new ArrayList<>();
+    private final List<T> list = new ArrayList<>();
+    private Function <T, Double> transform;
+    
+    public static void main(String... args) {
+        var list = List.of(5, 1, 2, 3, 1, 4, 5);
+        var stats = new SummaryStats(list, i -> i);
+        System.out.println(stats.max());
+        System.out.println(stats.min());
+        System.out.println(stats.average());
+    }
     
     public SummaryStats() {}
     
-    public SummaryStats(List<T> input) {
+    public SummaryStats(List<T> input, Function<T, Double> f) {
         list.addAll(input);
+        transform = f;
+    }
+    
+    public void setFunction(Function<T, Double> f) {
+        transform = f;
     }
     
     public void add(T number) {
@@ -31,13 +51,17 @@ public class SummaryStats<T extends Number> {
     
     public double average() {
         if (list.isEmpty()) throw new RuntimeException("no data!");
-        return list.stream().mapToDouble(i -> i.doubleValue()).average().getAsDouble();
+        return list.stream().mapToDouble(transform::apply).average().getAsDouble();
     }
     
     public double variance() {
         if (list.size() < 2) throw new RuntimeException("too little data!!!");
         var avg = average();
-        var EX2 = list.stream().mapToDouble(i -> {var t = i.doubleValue(); return t * t;}).average().getAsDouble();
+        var EX2 = list.stream()
+            .mapToDouble(i -> {var t = transform.apply(i); return t * t;})
+            .average()
+            .getAsDouble()
+        ;
         return EX2 - avg * avg;
     }
     
@@ -50,16 +74,29 @@ public class SummaryStats<T extends Number> {
     }
     
     public List<T> getData() {
-        return list.stream().collect(toList());
+        return new ArrayList<>(list);
     }
     
-    public double max() {
+    private Map.Entry<Double, List<T>> extreme(Comparator<Double> comp) { 
         if (list.isEmpty()) throw new RuntimeException("No data available!!!");
-        return list.stream().mapToDouble(i -> i.doubleValue()).max().getAsDouble();
+        var map = list.stream()
+            .collect(
+                groupingBy(transform::apply, 
+                           () -> new TreeMap<>(comp),
+                           mapping(s -> s, toList())
+                          )
+            )
+        ;
+        return map.firstEntry();                               
     }
     
-    public double min() {
+    public Map.Entry<Double, List<T>> max() {
         if (list.isEmpty()) throw new RuntimeException("No data available!!!");
-        return list.stream().mapToDouble(i -> i.doubleValue()).min().getAsDouble();
+        return extreme(Comparator.reverseOrder());
+    }
+    
+    public Map.Entry<Double, List<T>> min() {
+        if (list.isEmpty()) throw new RuntimeException("No data available!!!");
+        return extreme(Comparator.naturalOrder());
     }
 }
